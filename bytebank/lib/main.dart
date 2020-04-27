bd3f76c77d6b4c6a5e88af97e4dc5fbf184f12de
Victor Hugo <victorhugolgr@gmail.com>
2020-04-27 18:53:18 -0300

    Implementando o design do item de transferência

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,8 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Transferências'),),
+        body: ListaTransferencia(),
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),),
+          child: Icon(Icons.add),
+        ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(
+          Transferencia(1000, 222)
+        ),
+        ItemTransferencia(
+          Transferencia(222, 11)
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
