195f8df96ab3388d50bcab133dd3649db79b3a4e
Victor Hugo <victorhugolgr@gmail.com>
2020-05-04 17:32:35 -0300

    dashboard inicial

--- lista_contatos/lib/main.dart
+++ lista_contatos/lib/main.dart
@@ -7,10 +7,59 @@ class ListaContatosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
+      theme: ThemeData(
+        accentColor: Colors.yellowAccent[700],
       ),
-    ));
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/contact_book.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 40.0,
+                    ),
+                    Text(
+                      'Meus contatos',
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),,
+    );
   }
 }
