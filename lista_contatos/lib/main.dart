6fe52a678e61c2acbdb3ffa398a3f53d6906c688
Victor Hugo <victorhugolgr@gmail.com>
2020-05-07 16:45:18 -0300

    Lidando com os estados do snapshot

--- lista_contatos/lib/main.dart
+++ lista_contatos/lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lista_contatos/database/app_database.dart';
-import 'package:lista_contatos/models/contato.dart';
 import 'package:lista_contatos/screens/dashboard.dart';
 
-void main() {
-  runApp(ListaContatosApp());
-  debugPrint('===================================================');
-  save(Contato(0, 'Victor', '9132563994', 'victor.hugo@tu.nos')).then((id) {
-    findAll().then((contatos) => debugPrint(contatos.toString()));
-  });
-}
+void main() => runApp(ListaContatosApp());
 
 class ListaContatosApp extends StatelessWidget {
   // This widget is the root of your application.
--- lista_contatos/lib/screens/contacts_list.dart
+++ lista_contatos/lib/screens/contacts_list.dart
@@ -4,24 +4,46 @@ import 'package:lista_contatos/models/contato.dart';
 import 'package:lista_contatos/screens/contact_form.dart';
 
 class ContactList extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: FutureBuilder(
+      body: FutureBuilder<List<Contato>>(
+        initialData: List(),
         future: findAll(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          final List<Contato> contatos = snapshot.data;
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              final contato = contatos[index];
-              return _ContatoItem(contato);
-            },
-            itemCount: contatos.length,
-          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Carregando...')
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contato> contatos = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final contato = contatos[index];
+                  return _ContatoItem(contato);
+                },
+                itemCount: contatos.length,
+              );
+              break;
+          }
+
+          return Text('Unknown error!');
         },
       ),
       floatingActionButton: FloatingActionButton(
